5024aeb49631cd10480e1c2b8b5ed8bf34443ae3
Jody Septiawan <jody@Jodys-MacBook-Air.local>
2022-01-19 13:35:25 +0700

    update

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,22 @@
 // import model here
+const { user } = require('../../models');
 
 exports.addUsers = async (req, res) => {
-    // code here
-}
+  try {
+    const data = req.body;
+
+    const newData = await user.create(data);
+
+    res.send({
+      status: 'success',
+      message: 'Add user finished',
+      newData,
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'failed',
+      message: 'server error',
+    });
+  }
+};
